a13ecb04c973112b6a287735fe239d1f1c849c91
Anders Pedersen <hot415@hotmail.com>
2019-10-19 19:51:17 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,40 @@
-console.log("test");
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+function run() {
+  const client: github.GitHub = new github.GitHub(
+    core.getInput('repo-token', {required: true})
+  );
+  const context = github.context;
+  
+  if(context.payload.action !== 'opened') {
+    console.log('No issue or PR was opened, skipping');
+    return;
+  }
+  
+  // Do nothing if its not a pr or issue
+  const isIssue: boolean = !!context.payload.issue;
+  if (!isIssue && !context.payload.pull_request) {
+    console.log(
+      'The event that triggered this action was not a pull request or issue, skipping.'
+    );
+    return;
+  }
+  
+  if (!context.payload.sender) {
+    throw new Error('Internal error, no sender provided by GitHub');
+  }
+  
+  const issue: {owner: string; repo: string; number: number} = context.issue;
+  
+  await client.issues.close({
+    owner: issue.owner,
+    repo: issue.repo,
+    issue_number: issue.number,
+    body: 'test'
+  });
+  
+  console.log("done");
+}
+
+run();
