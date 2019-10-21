51fc85870e603b0755f0075d20e536935d1d520b
Anders Pedersen <hot415@hotmail.com>
2019-10-21 22:54:22 +0200

    Update test.js

--- test.js
+++ test.js
@@ -56,8 +56,6 @@ async function run() {
         throw new PackageError(`Package name already taken: ${package}`);
       }
       
-      const version = body[1] || (packageExists ? 'FIXME: increment minor version' : "1.0.0");
-      const validVersion = /^[0-9]+.[0-9]+.[0-9]+$/.test(version);//TODO: if !valid, do not calc sha1 or load more files. (throw an error or something)
       let sha1 = body[2] || await client.repos.listCommits({owner: owner, repo: repo, per_page: 1}).then(response => response.data[0].sha).catch(false);
       if (body[2]) {
         sha1 = client.repos.listCommits({owner: owner, repo: repo, per_page: 1, sha: body[2]}).then(response => response.data[0].sha).catch(e => false);
@@ -69,10 +67,17 @@ async function run() {
       }
       
       path = `./${directory[package] || formattedPackage}/au3pm.js`;
-      const packageDirectory = fs.existsSync(path) ? JSON.parse(fs.readFileSync(path)) : {};
+      
+      const packageDirectory = fs.existsSync(path) ? JSON.parse(fs.readFileSync(path)) : {repo: data.full_name, versions: {}};
+      const fIncrement = v => {let a = v.split(".");a[1]++;return a.join(".");};
+      const version = body[1] || (packageExists ? fIncrement(Object.keys(packageDirectory.versions).sort().reverse()[0] || "1.0.0") : "1.0.0");
       const versionExists = directory.hasOwnProperty(version);
+      const validVersion = /^[0-9]+.[0-9]+.[0-9]+$/.test(version);//TODO: if !valid, do not calc sha1 or load more files. (throw an error or something)
     
-      fs.writeFile(path, JSON.stringify(packageDirectory));
+      if (!versionExists) {
+        packageDirectory[version] = sha1;
+        fs.writeFile(path, JSON.stringify(packageDirectory));
+      }
       // console.log(data);
       
       client.issues.createComment({
